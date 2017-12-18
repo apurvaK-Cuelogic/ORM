7ae8d2769625166719a8d12ac7afb85755dc13c7
PratikB-Cuelogic <pratik.bamb@Cuelogic.com>
2017-12-18 15:39:15 +0530

    existing of Table checked

--- lib/adapter/mysqlOperation.rb
+++ lib/adapter/mysqlOperation.rb
@@ -7,10 +7,19 @@ class MysqlOperation
   end
 
   def all(con)
-    puts con.class
-    rs = con.query('select * from student')
+   # puts con.class
+    rs = con.query('select * from Student')
     rs.each_hash { |h| puts "#{h['id']},#{h['name']},#{h['rank']}"}
   end
+
+  def checkTable(tableName,con)
+    rs = con.query('show tables')
+    rs.each_hash { |h|
+    return true if h["Tables_in_ruby"].eql? tableName
+      false
+    }
+  end
+
 
   private_class_method :new
 end
--- lib/dbManager.rb
+++ lib/dbManager.rb
@@ -2,26 +2,41 @@ require_relative 'adapter/dbadapter'
 require_relative '../lib/adapter/mysqlOperation'
 
 class DbManager
+  @@subclass=nil
 
   read_properties=["all"]
+  def self.inherited(subclass)
+    @@subclass=subclass
+  end
+
+  def db_type
+    if @@con.class.eql? Mysql
+     @@db_type = 'Mysql'
+  end
 
   def initialize
     api= Api_model.new('mysql')
     @@con=api.connect
-
-    if @@con.class.eql? Mysql
-      #MysqlOperation.new
-      operation=MysqlOperation.instance
-      operation.all(@@con)
+    #if con= (connection is mySql or not)
+    if @@db_type.eql? Mysql
+      #check whether table exist
+      if MysqlOperation.instance.checkTable('Student',@@con).eql? true
+        @@valid=true
+      else
+        # if not then siplay message and abort
+        @@valid=false
+        abort("Table does not exist")
+      end
     end
   end
 
-  class << self
-    def all
-
+  def self.all
+    if @@db_type.class.eql? Mysql
+    MysqlOperation.instance.all(@@con)
     end
-
   end
+
+
 end
 
 
